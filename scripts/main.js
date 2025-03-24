359d9b0d772852a5a9ba7e8b8008f0b504729caa
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-24 18:50:01 +0330

    feat: implement operate function

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,26 @@ let divide = (a, b) => a / b;
 let firstOperand = 0;
 let operator = "";
 let secondOperand = 0;
+
+function operate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(+firstNumber, +secondNumber);
+            break;
+
+        case "-":
+            return substract(+firstNumber, +secondNumber);
+            break;
+
+        case "*":
+            return multiply(+firstNumber, +secondNumber);
+            break;
+
+        case "/":
+            return divide(+firstNumber, +secondNumber);
+            break;
+
+        default:
+            break;
+    }
+}
