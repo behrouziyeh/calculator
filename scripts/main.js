01139deb6c0364b834cae2fd879c9c338ce7ea1f
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-24 18:46:45 +0330

    feat: create variables for operands and operator

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,7 @@ let add = (a, b) => a + b;
 let substract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+
+let firstOperand = 0;
+let operator = "";
+let secondOperand = 0;
