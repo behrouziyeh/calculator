759f5086a705dc9d2cf474e61f1375bc5ac73ce3
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-25 09:57:56 +0330

    feat: make the calculator buttons work

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 let remainder = (a, b) => a % b;
 
-let firstOperand = 0;
+let firstOperand = "";
 let operator = "";
-let secondOperand = 0;
+let secondOperand = "";
 
 function operate(firstNumber, operator, secondNumber) {
     switch (operator) {
@@ -34,3 +34,70 @@ function operate(firstNumber, operator, secondNumber) {
             break;
     }
 }
+
+const calculator = document.querySelector(".calculator");
+
+const userInputDisplay = calculator.querySelector(".calculator__user-input");
+const resultDisplay = calculator.querySelector(".calculator__result");
+
+const digits = calculator.querySelectorAll(".calculator__digit");
+const operatorsButtons = calculator.querySelectorAll(".calculator__operator");
+const allClearButton = calculator.querySelector(".calculator__all-clear");
+const clearButton = calculator.querySelector(".calculator__clear");
+const calculateButton = calculator.querySelector(".calculator__equals");
+
+function updateDisplay() {
+    userInputDisplay.textContent = `${firstOperand} ${operator} ${secondOperand}`;
+    resultDisplay.textContent = "";
+}
+
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => {
+        if (operator !== "") {
+            secondOperand = secondOperand + String(digit.value);
+        } else {
+            firstOperand = firstOperand + String(digit.value);
+        }
+        updateDisplay();
+    });
+});
+
+operatorsButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+        if (secondOperand) {
+            firstOperand = operate(firstOperand, operator, secondOperand);
+            operator = operatorButton.value;
+            secondOperand = "";
+        } else if (firstOperand) {
+            operator = operatorButton.value;
+        }
+        updateDisplay();
+    });
+});
+
+allClearButton.addEventListener("click", () => {
+    firstOperand = operator = secondOperand = "";
+    updateDisplay();
+});
+
+clearButton.addEventListener("click", () => {
+    if (secondOperand) {
+        secondOperand = secondOperand.split("").slice(0, -1).join("");
+    } else if (operator) {
+        operator = "";
+    } else if (firstOperand) {
+        firstOperand = firstOperand.split("").slice(0, -1).join("");
+    }
+    updateDisplay();
+});
+
+calculateButton.addEventListener("click", () => {
+    if (firstOperand && operator && secondOperand) {
+        resultDisplay.textContent = operate(
+            firstOperand,
+            operator,
+            secondOperand
+        );
+        firstOperand = operator = secondOperand = "";
+    }
+});
